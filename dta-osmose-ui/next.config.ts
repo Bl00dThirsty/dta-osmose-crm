7de4bfd44d98dc921012da921218c66f2c3e8156
John MANGA <john.manga@dta-alliance.com>
2025-06-24 11:35:47 +0100

    Bug-allowedDevOrigins

--- dta-osmose-ui/next.config.ts
+++ dta-osmose-ui/next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: ['local-origin.dev', '*.local-origin.dev', 'http://192.168.1.6:3000'],
+  // Add any other Next.js configuration options here
 };
 
 export default nextConfig;
